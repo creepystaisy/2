85efa0fd218e107132bc53802b69f21606aa9459
example <email@example.com>
2020-07-21 11:58:45 +0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -40,14 +40,21 @@ table = webix.ui ({
                     let stock_id = stock.findIndex(function (item) {
                     return item.id === id
                     });
-                    stock[stock_id].quantity--;
-                    $$("stock_table").refresh();
-                   
-                   
-
                     let cart_id = cart.findIndex(function (item) {
                     return item.id === id
                     });
+
+                    if(stock[stock_id].quantity<=1)
+                    {
+                        stock.splice(stock_id, 1);
+                        $$("stock_table").remove(this.getSelectedId(true).join());
+                    }else{
+                        stock[stock_id].quantity--;
+                        $$("stock_table").refresh();
+                    }
+                   
+                    stock.forEach(element => console.log(element));
+                    
                     cart[cart_id].quantity++;
                     $$("cart_table").refresh();
                     
@@ -83,13 +90,20 @@ table = webix.ui ({
                     let cart_id = cart.findIndex(function (item) {
                     return item.id === id
                     });
-                    cart[cart_id].quantity--;
-                    $$("cart_table").refresh();
-
-
                     let stock_id = stock.findIndex(function (item) {
                     return item.id === id
                     });
+                    
+                    if(cart[cart_id].quantity<=1)
+                    {
+                        cart.splice(cart_id, 1);
+                        $$("cart_table").remove(this.getSelectedId(true).join());
+                    }else{
+                    cart[cart_id].quantity--;
+                    $$("cart_table").refresh();
+                    }
+
+                    
                     stock[stock_id].quantity++;
                     $$("stock_table").refresh();
                    
